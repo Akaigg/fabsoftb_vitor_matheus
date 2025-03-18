e65fee9cad1bfa4e5af62b687aaa7a85624954ee
VITOR MACHADO BLUME <vitor.blume@univille.br>
2025-03-17 21:59:22 -0300

    feat: aula

--- revisao_vitor/src/main/java/br/univille/Main.java
+++ revisao_vitor/src/main/java/br/univille/Main.java
@@ -4,9 +4,12 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
 
-        Cliente zezinho = new Cliente();
-        zezinho.setNome("zezinho da silva Sauro");
-        Cliente mariazinha = new Cliente();    
+        Cliente zezinho = new Cliente("Zeninho");
+        zezinho.setNome("Zezinho");
+        Cliente mariazinha = new Cliente("mariazinha");    
         mariazinha.setNome("Mariazinha");
+
+        System.out.println(zezinho);
+        System.out.println(mariazinha);
     }
 }
