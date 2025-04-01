78f0093df91edce4bb942ef2bcfad83ab05ebec3
MATHEUS KARPINSKI <matheus.karpinski@univille.br>
2025-04-01 20:28:10 -0300

    feat:inicioprojeto

---
+++ projfabsoftlavanderia/src/main/java/br/univille/projfabsoftlavanderia/ProjfabsoftlavanderiaApplication.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsoftlavanderia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjfabsoftlavanderiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjfabsoftlavanderiaApplication.class, args);
+	}
+
+}
---
+++ projfabsoftlavanderia/src/test/java/br/univille/projfabsoftlavanderia/ProjfabsoftlavanderiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsoftlavanderia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjfabsoftlavanderiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
