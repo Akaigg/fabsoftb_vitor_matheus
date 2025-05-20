ab47d54205196ea80028fc831a8e91c3a8bc964a
Vitor Blume <124194664+Akaigg@users.noreply.github.com>
2025-05-20 23:41:27 +0000

    feat: frontend

---
+++ projfabsoft-frontend/src/app/service/cliente.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import {Cliente} from '../model/cliente';
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  constructor() { }
+}
