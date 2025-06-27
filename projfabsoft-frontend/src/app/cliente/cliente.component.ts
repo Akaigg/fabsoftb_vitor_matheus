e93e61a555af98256965eb5e3755572bbd240c0b
MatheusKarpinski <karpinskimatheus@gmail.com>
2025-06-27 20:22:12 -0300

    feat:excluir

--- projfabsoft-frontend/src/app/cliente/cliente.component.ts
+++ projfabsoft-frontend/src/app/cliente/cliente.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import {Cliente} from '../model/cliente';
-import {ClienteService} from '../service/cliente.service';
-import {HttpClientModule} from '@angular/common/http';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Cliente } from '../model/cliente';
+import { ClienteService } from '../service/cliente.service';
+import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-cliente',
@@ -13,26 +14,55 @@ import { Router } from '@angular/router';
   providers: [ClienteService, Router]
 })
 export class ClienteComponent {
-    listaClientes: Cliente[] = [];
+  listaClientes: Cliente[] = [];
 
-    constructor(
-      private clienteService: ClienteService, 
-      private router:Router
-    ) {}
+  @ViewChild('myModal') modalElement!: ElementRef;
+  private modal!: bootstrap.Modal;
+  private clienteSelecionado!: Cliente;
 
-    novo(){
-      this.router.navigate(['clientes/novo'])
-    }
+  constructor(
+    private clienteService: ClienteService, 
+    private router: Router
+  ) {}
 
+  novo() {
+    this.router.navigate(['clientes/novo']);
+  }
 
-    ngOnInit(){
-      console.log("Carregando clientes...");
-      this.clienteService.getClientes().subscribe (clientes => {
-        this.listaClientes = clientes;
-        }
-      );
-    }
-    alterar(cliente:Cliente){
-      this.router.navigate(['clientes/alterar', cliente.id]);
-    }
+  ngOnInit() {
+    console.log("Carregando clientes...");
+    this.clienteService.getClientes().subscribe(clientes => {
+      this.listaClientes = clientes;
+    });
+  }
+
+  alterar(cliente: Cliente) {
+    this.router.navigate(['clientes/alterar', cliente.id]);
+  }
+
+  abrirConfirmacao(cliente: Cliente) {
+    this.clienteSelecionado = cliente;
+    this.modal = new bootstrap.Modal(this.modalElement.nativeElement);
+    this.modal.show();
+  }
+
+  fecharConfirmacao() {
+    this.modal.hide();
+  }
+
+  confirmarExclusao() {
+    this.clienteService.excluirCliente(this.clienteSelecionado.id).subscribe(
+      () => {
+        this.fecharConfirmacao();
+        this.clienteService.getClientes().subscribe(
+          clientes => {
+            this.listaClientes = clientes;
+          }
+        );
+      },
+      error => {
+        console.error('Erro ao excluir cliente:', error);
+      }
+    );
+  }
 }
